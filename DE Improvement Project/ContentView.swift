d70dd3a6a4b2ec5ef5bc910345a2474272f7fab7
churroTheDuck <152838803+churroTheDuck@users.noreply.github.com>
2024-05-07 09:16:22 -0700

    Initial Commit

---
+++ DE Improvement Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DE Improvement Project
+//
+//  Created by NMS16446-7-lanchen on 5/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DE Improvement Project/DE_Improvement_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DE_Improvement_ProjectApp.swift
+//  DE Improvement Project
+//
+//  Created by NMS16446-7-lanchen on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DE_Improvement_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
